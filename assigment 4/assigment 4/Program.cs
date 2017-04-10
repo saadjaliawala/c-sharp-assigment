26d51b54dcfcb344daf99183651c7a8452dd57df
saadjaliawala <saadmehmood2012@gmail.com>
2017-04-11 00:34:08 +0500

    all c# work uptill now

---
+++ assigment 4/assigment 4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace assigment_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  a, b =0;
+            
+            a = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i <= a; i++)
+if (i % 2 != 0)
+{
+
+    b = b + i;
+    if (i == a)
+    {
+
+        Console.WriteLine(i);
+        Console.WriteLine("the sum of all odd numbers are below");
+        Console.WriteLine( b);
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+
+}
+Console.ReadLine();
+}
+
+        
+    }
+}   
---
+++ assigment 6/assigment 6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace assigment_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c = 0;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            for (int  i = a; a <= b; a++) {
+                if (a % 2 != 0) {
+                    c = c + a;
+                    if (a >= b)
+                    {
+                        Console.WriteLine(a);
+                        Console.WriteLine("the sum is ");
+                        Console.WriteLine(c);
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine(a);
+                       // Console.ReadLine();
+                    }
+                }
+              
+            }
+        }
+    }
+}
